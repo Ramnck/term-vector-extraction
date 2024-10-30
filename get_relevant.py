076ead788fbf10c2109f70acb71012106480edc3
Ramnck <r.karet@gmail.com>
2024-10-30 10:08:30 +0300

    Api closed

--- get_relevant.py
+++ get_relevant.py
@@ -141,8 +141,4 @@ if __name__ == "__main__":
 
     coro = main(api, args.number, args.input, args.output, args.num_of_workers)
     asyncio.run(coro)
-
-    try:
-        asyncio.run(api.close())
-    except:
-        pass
+    api.close()
